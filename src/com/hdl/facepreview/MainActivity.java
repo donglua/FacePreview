63ee368fce761f2b1285e291342d1c26e2d62e8a
donglua <hdlxxc@gmail.com>
2013-07-14 17:06:58 +0800

    first commit

---
+++ src/com/hdl/facepreview/MainActivity.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (C) 2010,2011,2012 Samuel Audet
+ *
+ * FacePreview - A fusion of OpenCV's facedetect and Android's CameraPreview samples,
+ *               with JavaCV + JavaCPP as the glue in between.
+ *
+ * This file was based on CameraPreview.java that came with the Samples for 
+ * Android SDK API 8, revision 1 and contained the following copyright notice:
+ *
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * IMPORTANT - Make sure the AndroidManifest.xml file looks like this:
+ *
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+ *     package="com.googlecode.javacv.facepreview"
+ *     android:versionCode="1"
+ *     android:versionName="1.0" >
+ *     <uses-sdk android:minSdkVersion="4" />
+ *     <uses-permission android:name="android.permission.CAMERA" />
+ *     <uses-feature android:name="android.hardware.camera" />
+ *     <application android:label="@string/app_name">
+ *         <activity
+ *             android:name="FacePreview"
+ *             android:label="@string/app_name"
+ *             android:screenOrientation="landscape">
+ *             <intent-filter>
+ *                 <action android:name="android.intent.action.MAIN" />
+ *                 <category android:name="android.intent.category.LAUNCHER" />
+ *             </intent-filter>
+ *         </activity>
+ *     </application>
+ * </manifest>
+ */
+
+package com.hdl.facepreview;
+
+import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_8U;
+import static com.googlecode.javacv.cpp.opencv_core.cvClearMemStorage;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSeqElem;
+import static com.googlecode.javacv.cpp.opencv_core.cvLoad;
+import static com.googlecode.javacv.cpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNING;
+import static com.googlecode.javacv.cpp.opencv_objdetect.cvHaarDetectObjects;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.googlecode.javacpp.Loader;
+import com.googlecode.javacv.cpp.opencv_core.CvMemStorage;
+import com.googlecode.javacv.cpp.opencv_core.CvRect;
+import com.googlecode.javacv.cpp.opencv_core.CvSeq;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_objdetect;
+import com.googlecode.javacv.cpp.opencv_objdetect.CvHaarClassifierCascade;
+
+// ----------------------------------------------------------------------
+
+public class MainActivity extends Activity {
+    private FrameLayout layout;
+    private FaceView faceView;
+    private Preview mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		}
+        // Create our Preview view and set it as the content of our activity.
+        try {
+            layout = new FrameLayout(this);
+            faceView = new FaceView(this);
+            mPreview = new Preview(this, faceView);
+            layout.addView(mPreview);
+            layout.addView(faceView);
+            setContentView(layout);
+		} catch (IOException e) {
+			e.printStackTrace();
+			new AlertDialog.Builder(this).setMessage(e.getMessage()).create()
+					.show();
+		}
+    }
+}
+
+// ----------------------------------------------------------------------
+
+class FaceView extends View implements Camera.PreviewCallback {
+    public static final int SUBSAMPLING_FACTOR = 4;
+
+    private IplImage grayImage;
+    private CvHaarClassifierCascade classifier;
+    private CvMemStorage storage;
+    private CvSeq faces;
+
+    public FaceView(MainActivity context) throws IOException {
+        super(context);
+
+        // Load the classifier file from Java resources.
+        File classifierFile = Loader.extractResource(getClass(),
+            "/com/hdl/facepreview/haarcascade_frontalface_alt.xml",
+            context.getCacheDir(), "classifier", ".xml");
+        if (classifierFile == null || classifierFile.length() <= 0) {
+            throw new IOException("Could not extract the classifier file from Java resource.");
+        }
+
+        // Preload the opencv_objdetect module to work around a known bug.
+        Loader.load(opencv_objdetect.class);
+        classifier = new CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
+        classifierFile.delete();
+        if (classifier.isNull()) {
+            throw new IOException("Could not load the classifier file.");
+        }
+        storage = CvMemStorage.create();
+    }
+
+    public void onPreviewFrame(final byte[] data, final Camera camera) {
+        try {
+            Camera.Size size = camera.getParameters().getPreviewSize();
+            processImage(data, size.width, size.height);
+            camera.addCallbackBuffer(data);
+        } catch (RuntimeException e) {
+            // The camera has probably just been released, ignore.
+        }
+    }
+
+    protected void processImage(byte[] data, int width, int height) {
+        // First, downsample our image and convert it into a grayscale IplImage
+        int f = SUBSAMPLING_FACTOR;
+        if (grayImage == null || grayImage.width() != width/f || grayImage.height() != height/f) {
+            grayImage = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 1);
+        }
+        int imageWidth  = grayImage.width();
+        int imageHeight = grayImage.height();
+        int dataStride = f*width;
+        int imageStride = grayImage.widthStep();
+        ByteBuffer imageBuffer = grayImage.getByteBuffer();
+        for (int y = 0; y < imageHeight; y++) {
+            int dataLine = y*dataStride;
+            int imageLine = y*imageStride;
+            for (int x = 0; x < imageWidth; x++) {
+                imageBuffer.put(imageLine + x, data[dataLine + f*x]);
+            }
+        }
+
+        cvClearMemStorage(storage);
+        faces = cvHaarDetectObjects(grayImage, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+        postInvalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setTextSize(20);
+
+        String s = "FacePreview - This side up.";
+        float textWidth = paint.measureText(s);
+        canvas.drawText(s, (getWidth()-textWidth)/2, 20, paint);
+
+        if (faces != null) {
+            paint.setStrokeWidth(2);
+            paint.setStyle(Paint.Style.STROKE);
+            float scaleX = (float)getWidth()/grayImage.width();
+            float scaleY = (float)getHeight()/grayImage.height();
+            int total = faces.total();
+            for (int i = 0; i < total; i++) {
+                CvRect r = new CvRect(cvGetSeqElem(faces, i));
+                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
+                canvas.drawRect(x*scaleX, y*scaleY, (x+w)*scaleX, (y+h)*scaleY, paint);
+            }
+        }
+    }
+}
